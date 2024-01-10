8da2f591eb11cd2be91f592c3f6534fdc36eb98d
Stilyan <117260079+stilyan122@users.noreply.github.com>
2024-01-10 17:23:40 +0200

    Add files via upload

--- ElementFactory/wwwroot/js/listItems.js
+++ ElementFactory/wwwroot/js/listItems.js
@@ -1,3 +1,13 @@
 function LoadLists() {
-    console.log(1);
+    var select = document.getElementsByClassName("seeItemsList")[0];   
+    console.log(select);
+        select
+        .addEventListener('change', function (event) {
+            event.preventDefault();
+            var items = document.getElementsByTagName("td");
+            for (var i in items) {
+                console.log(i.children);
+            }
+        });
+    console.log(2);
 }
