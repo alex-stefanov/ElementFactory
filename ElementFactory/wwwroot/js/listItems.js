dcf4bb9d3b5a9a1ba7f46d9997c7461f5d549ce5
Stilyan <117260079+stilyan122@users.noreply.github.com>
2024-01-13 09:18:15 +0200

    Add files via upload

--- ElementFactory/wwwroot/js/listItems.js
+++ ElementFactory/wwwroot/js/listItems.js
@@ -1,8 +1,35 @@
 function PageSettings(atomicMasses) {
-    console.log(atomicMasses);
-    var select = document.getElementById('seeItemsList');   
+
+    var select = document.getElementById('seeItemsList'); 
+
     function StyleCell(element) {
-        element.classList = [];
+        var array = Array.from(element.classList);
+        var cellClass = array[0];
+        //element.classList.remove(cellClass);
+        console.log(element.classList);
+        element.classList.remove(cellClass);
+        console.log(element.classList);
+       
+    }
+
+    function LinearInterp(start, end, percentage) {
+        start + Math.round(percentage * (end - start));
+    }
+
+    function ColorInterp(r, g, b) {
+        return "rgb(" + LinearInterp(r) + "," + LinearInterp(g) + "," + LinearInterp(b) + ")";
+    }
+
+    function GradientPick(start, end, percentage) {
+        if (percentage < 0.5) {
+            return ColorInterp(start, "red", percentage / 0.5);
+        }
+        else if (percentage == 0.5) {
+            return "blue";
+        }
+        else {
+            return ColorInterp("blue", "green", (percentage - 0.5) / 0.5);
+        }
     }
 
     select
@@ -13,8 +40,6 @@ function PageSettings(atomicMasses) {
         var cellsCollection = document.getElementsByTagName('td');
         var cellsCollectionArray = Array.from(cellsCollection)
             .filter(x => x.classList.length != 0);
-
-        console.log(array);
 
         var selectedIndex = select.selectedIndex;
         var selectedOption = select.options[selectedIndex].id;
@@ -27,7 +52,14 @@ function PageSettings(atomicMasses) {
 
             
             } else if (selectedOption == 'seeAtomicMass') {
-                
+ 
+                    var mass = atomicMasses[i];                    
+
+                    StyleCell(currentCell);
+
+                console.log(GradientPick(0, 255, mass)); 
+                console.log(currentCell);
+                    currentCell.style.backgroundColor = GradientPick(0, 255, mass);
             } else if (selectedOption == 'seePhysicalAppearance') {
 
             } else if (selectedOption == 'seeAtomicRadius') {
