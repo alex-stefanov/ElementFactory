bca247820aebd07689e95ac8da20b44694edb903
alex <rlgalexbgto@gmail.com>
2024-01-15 23:06:53 +0200

    Major changes in table structures and css update:)

--- ElementFactory/wwwroot/js/listItems.js
+++ ElementFactory/wwwroot/js/listItems.js
@@ -1,18 +1,8 @@
 function PageSettings(
-    atomicNumbers,
-    chemicalSymbols,
-    elementNames,
-    chemicalTypeNames,
-    atomicMasses,
-    states,
-    atomicRadiuses,
-    meltingPoints,
-    boilingPoints,
-    years) {
-
-    function CreateDOMElement(tagName, parent, className) {
+    model) {
+    function CreateDOMElement(tagName, parent, id) {
         var element = document.createElement(tagName);
-        element.classList.add(className);
+        element.id = id;
         parent.innerHTML += element;
         return element;
     }
@@ -34,88 +24,159 @@ function PageSettings(
         }, 3000);
     }
 
-    var cellsCollection = document.getElementsByTagName('td');
+    function CreateTableRows(number, table, textContent, elementProperty) {
 
-    var cellsCollectionArray = Array.from(cellsCollection)
-        .filter(x => x.classList.length != 0).sort((a, b) =>
-            a.getElementsByClassName('atomicNumber')[0].textContent -
-            b.getElementsByClassName('atomicNumber')[0].textContent);
+        var tr = CreateDOMElement('tr', table, `popUpTr${number}`);
 
-    var yearInputField = document.getElementsByClassName('yearInput')[0];
-    var elementInputField = document.getElementsByClassName('elementInput')[0];
-    var darkModeBtn = document.getElementsByClassName('darkModeBtn')[0];
+        var th = CreateDOMElement('th', tr, `popUpTh${number}`);
 
-    //TODO: -> structure
-    for (var i = 0; i < cellsCollectionArray.length; i++) {
+        var p = CreateDOMElement('p', th, `popUpP${number}`);
+        p.textContent = textContent;
 
-        var currentCell = cellsCollectionArray[i];
+        var td = CreateDOMElement('td', tr, `popUpTd${number}`);
 
-        currentCell.addEventListener('click', function (event) {
-            event.preventDefault();
-
-            var popUpContainer = CreateDOMElement('div', document.getElementsByTagName('body')[0], 'popUpContainer');
-            popUpContainer.style.display = 'none';
-
-            var main = CreateDOMElement('main', popUpContainer, 'popUpMain');
-
-            var header = CreateDOMElement('header', main, 'popUpHeader');
-
-            var article1 = CreateDOMElement('article', header, 'popUpHeaderArticle1');
-            article1.textContent = 'Свойства на елемента';
-
-            var article2 = CreateDOMElement('article', main, 'popUpMainArticle2');
-
-            var section1 = CreateDOMElement('section', article2, 'popUpMainSection1');
-
-            var h2_1 = CreateDOMElement('h2', section1, 'popUpMainArticleSectionH2_1');
-            h2_1.textContent = atomicNumbers[i];
-
-            var h1_1 = CreateDOMElement('h1', section1, 'popUpMainArticleSectionH1_1');
-            h1_1.textContent = chemicalSymbols[i];
-
-            var p1 = CreateDOMElement('p', section1, 'popUpMainArticleSectionP1');
-            p1.textContent = elementNames[i];
-
-            var p2 = CreateDOMElement('p', section1, 'popUpMainArticleSectionP2');
-            p2.textContent = chemicalTypeNames[i];
-
-
-        })
+        var p1 = CreateDOMElement('p', td, `popUpP${number + 1}`);
+        p1.textContent = elementProperty;
     }
-    //
 
-    var select = document.getElementById('seeItemsList');
+    function PopulateTable(table, currentElement, counter) {
 
-    select.addEventListener('change', function (event) {
-          event.preventDefault();
+        //First Row
+        CreateTableRows(counter, table, 'Агрегатно състояние:', currentElement.State);
+        counter += 2;
 
-        var selectedIndex = select.selectedIndex;
-        var selectedOption = select.options[selectedIndex].id;
-        yearInputField.style.display = 'none';
-        elementInputField.style.display = 'none';
+        //Second Row
+        CreateTableRows(counter, table, 'Период:', currentElement.Period);
+        counter += 2;
+
+        //Third Row
+        CreateTableRows(counter, table, 'Група:', currentElement.Group);
+        counter += 2;
+
+        //Fourth Row
+        CreateTableRows(counter, table, 'Електроотрицателност:', currentElement.Electronegativity);
+        counter += 2;
+
+        //Fifth Row
+        CreateTableRows(counter, table, 'Атомна Маса:', currentElement.AtomicMass);
+        counter += 2;
+
+        //Sixth Row
+        CreateTableRows(counter, table, 'Атомен Радиус:', currentElement.AtomicRadius);
+        counter += 2;
+
+        //Seventh Row
+        CreateTableRows(counter, table, 'Плътност:', currentElement.Density);
+        counter += 2;
+
+        //Eighth Row
+        CreateTableRows(counter, table, 'Температура на топене:', currentElement.MeltingPoint);
+        counter += 2;
+
+        //Ninth Row
+        CreateTableRows(counter, table, 'Температура на кипене:', currentElement.BoilingPoint);
+        counter += 2;
+
+        //Tenth Row
+        CreateTableRows(counter, table, 'Електронни Слоеве:', currentElement.ElectronicLayers);
+        counter += 2;
+
+        //Eleventh Row
+        if (currentElement.IsRadioactive) {
+            CreateTableRows(counter, table, 'Радиоктивен:', 'Да');
+        }
+        else {
+            CreateTableRows(counter, table, 'Радиоктивен:', 'Не');
+        }
+        counter += 2;
+
+        //Twelfth Row
+        if (currentElement.IsSynthetic) {
+            CreateTableRows(counter, table, 'Синтетичен:', 'Да');
+        }
+        else {
+            CreateTableRows(counter, table, 'Синтетичен:', 'Не');
+        }
+    }
+
+    function CreatePopUpMenu(cellsCollectionArray, values) {
 
         for (var i = 0; i < cellsCollectionArray.length; i++) {
 
-                var currentCell = cellsCollectionArray[i];
+            var currentCell = cellsCollectionArray[i];
 
+            var currentElement = values[i];
+
+            currentCell.addEventListener('mouseover', function (event) {
+                console.log(1);
+                event.preventDefault();
+               
+                var popUpContainer = CreateDOMElement('div', document.getElementsByTagName('body')[0], 'popUpContainer');
+                popUpContainer.style.display = 'none';
+
+                var main = CreateDOMElement('main', popUpContainer, 'popUpMain');
+
+                var header = CreateDOMElement('header', main, 'popUpHeader');
+
+                var article1 = CreateDOMElement('article', header, 'popUpArticle1');
+                article1.textContent = 'Свойства на елемента';
+
+                var article2 = CreateDOMElement('article', main, 'popUpArticle2');
+
+                var section1 = CreateDOMElement('section', article2, 'popUpSection1');
+
+                var h2_1 = CreateDOMElement('h2', section1, 'popUpH2_1');
+                h2_1.textContent = currentElement.AtomicNumber;
+
+                var h1_1 = CreateDOMElement('h1', section1, 'popUpH1_1');
+                h1_1.textContent = currentElement.Symbol;
+
+                var p1 = CreateDOMElement('p', section1, 'popUpP1');
+                p1.textContent = currentElement.Name;
+
+                var p2 = CreateDOMElement('p', section1, 'popUpP2');
+                p2.textContent = currentElement.ChemicalType.Name;
+
+                var section2 = CreateDOMElement('section', article2, 'popUpSection2');
+
+                var table = CreateDOMElement('table', section2, 'popUpTable');
+
+                PopulateTable(table, currentElement, 3);
+        })
+        }
+    }
+
+    function CreateEventListeners(cellsCollectionArray, values, select,
+        yearInputField, elementInputField, darkModeBtn) {
+
+        select.addEventListener('change', function (event) {
+            event.preventDefault();
+
+            var selectedIndex = select.selectedIndex;
+            var selectedOption = select.options[selectedIndex].id;
+            yearInputField.style.display = 'none';
+            elementInputField.style.display = 'none';
+
+            for (var i = 0; i < cellsCollectionArray.length; i++) {
+
+                var currentCell = cellsCollectionArray[i];
+                var currentValue = values[i];
                 if (selectedOption == 'seeAtomicNumber') {
 
-                    var chemicalType = chemicalTypeNames[i]
-                        .map(x => String.fromCharCode(x)).join('');
-
+                    var chemicalType = currentValue.ChemicalType.Name;
                     UpdateElement(currentCell, null);
 
-                    for (var j = 0; j < chemicalType.split(' ').length; j++) { 
+                    for (var j = 0; j < chemicalType.split(' ').length; j++) {
                         currentCell.classList.add(chemicalType.split(' ')[j]);
                     }
                 }
 
                 else if (selectedOption == 'seeAtomicMass') {
- 
-                    var mass = atomicMasses[i] * 0.60;  
-                
+
+                    var mass = currentValue.AtomicMass * 0.60;
+
                     UpdateElement(currentCell, 'hasAtomicMass');
-                
+
 
                     currentCell.style.backgroundColor = `rgba(115, ${235 - mass}, 255)`;
 
@@ -124,15 +185,14 @@ function PageSettings(
 
                 else if (selectedOption == 'seePhysicalAppearance') {
 
-                    var state = states[i]
-                        .map(x => String.fromCharCode(x)).join('');
+                    var state = currentValue.State;
 
                     UpdateElement(currentCell, state);
                 }
 
                 else if (selectedOption == 'seeAtomicRadius') {
 
-                    var atomicRadius = parseFloat(atomicRadiuses[i]) / parseFloat(5);
+                    var atomicRadius = parseFloat(currentValue.AtomicRadius) / parseFloat(5);
 
                     UpdateElement(currentCell, 'hasAtomicRadius');
 
@@ -151,7 +211,7 @@ function PageSettings(
 
                 else if (selectedOption == 'seeMeltingPoint') {
 
-                    var meltingPoint = parseFloat(meltingPoints[i]) / parseFloat(31);
+                    var meltingPoint = parseFloat(currentValue.MeltingPoint) / parseFloat(31);
 
                     UpdateElement(currentCell, 'hasMeltingPoint');
 
@@ -161,119 +221,138 @@ function PageSettings(
                 }
 
                 else if (selectedOption == 'seeBoilingPoint') {
-                var boilingPoint = parseFloat(boilingPoints[i]) / parseFloat(31);
+                    var boilingPoint = parseFloat(currentValue.BoilingPoint) / parseFloat(31);
 
-                UpdateElement(currentCell, 'hasBoilingPoint');
+                    UpdateElement(currentCell, 'hasBoilingPoint');
 
-                currentCell.style.backgroundColor = `rgba(${boilingPoint}, 0, 0)`;
+                    currentCell.style.backgroundColor = `rgba(${boilingPoint}, 0, 0)`;
 
-                currentCell.style.border = `3px solid rgba(${boilingPoint * 0.6}, 0, 0)`;
-            }
+                    currentCell.style.border = `3px solid rgba(${boilingPoint * 0.6}, 0, 0)`;
+                }
 
                 else if (selectedOption == 'seeByYear') {
                     yearInputField.style.display = 'inline-block';
                     break;
-                } 
+                }
 
                 else if (selectedOption == 'seeByName') {
                     elementInputField.style.display = 'inline-block';
                     break;
                 }
-        }
-    });
-
-    yearInputField.addEventListener('focus', function (event) {
-        for (var i = 0; i < cellsCollectionArray.length; i++) {
-            var chemicalType = chemicalTypeNames[i]
-                .map(x => String.fromCharCode(x)).join('');
-
-            var currentCell = cellsCollectionArray[i];
-
-            UpdateElement(currentCell, null);
-
-            for (var j = 0; j < chemicalType.split(' ').length; j++) {
-                currentCell.classList.add(chemicalType.split(' ')[j]);
             }
-        }
-    });
 
-    yearInputField.addEventListener('blur', function (event) {
-        event.preventDefault();
+        });
 
-        var yearValue = yearInputField.value;
-
-        if (!yearValue || yearValue == '' || yearValue == ' ' || yearValue < 0 || yearValue > 2024) {
-            ShowElementError(1);
-        }
-        else {
+        yearInputField.addEventListener('focus', function (event) {
             for (var i = 0; i < cellsCollectionArray.length; i++) {
-                var currYear = years[i];
+                var chemicalType = values[i].ChemicalType.Name;
 
-                if (currYear > yearValue) {
-                    var cell = cellsCollectionArray[i];
-                    UpdateElement(cell, 'hasNotYear');
+                var currentCell = cellsCollectionArray[i];
+
+                UpdateElement(currentCell, null);
+
+                for (var j = 0; j < chemicalType.split(' ').length; j++) {
+                    currentCell.classList.add(chemicalType.split(' ')[j]);
                 }
             }
-        }
-    });
+        });
 
-    elementInputField.addEventListener('focus', function (event) {
-        for (var i = 0; i < cellsCollectionArray.length; i++) {
-            var chemicalType = chemicalTypeNames[i]
-                .map(x => String.fromCharCode(x)).join('');
+        yearInputField.addEventListener('blur', function (event) {
+            event.preventDefault();
 
-            var currentCell = cellsCollectionArray[i];
+            var yearValue = yearInputField.value;
 
-            UpdateElement(currentCell, null);
-
-            for (var j = 0; j < chemicalType.split(' ').length; j++) {
-                currentCell.classList.add(chemicalType.split(' ')[j]);
+            if (!yearValue || yearValue == '' || yearValue == ' ' || yearValue < 0 || yearValue > 2024) {
+                ShowElementError(1);
             }
-        }
-    });
+            else {
+                for (var i = 0; i < cellsCollectionArray.length; i++) {
+                    var currYear = values[i].YearFound;
 
-    elementInputField.addEventListener('blur', function (event) {
-        event.preventDefault();
+                    if (currYear > yearValue) {
+                        var cell = cellsCollectionArray[i];
+                        UpdateElement(cell, 'hasNotYear');
+                    }
+                }
+            }
+        });
 
-        var elementIsFound = false;
-        var elementValue = elementInputField.value;
-
-        if (!elementInputField || elementInputField == '' || elementInputField == ' ') {
-            ShowElementError(2);
-        }
-        else {
+        elementInputField.addEventListener('focus', function (event) {
             for (var i = 0; i < cellsCollectionArray.length; i++) {
+                var chemicalType = values[i].ChemicalType.Name;
 
-                var currName = elementNames[i].map(x => String.fromCharCode(x)).join('');;
-                
-                if (currName != elementValue) {
-                    var cell = cellsCollectionArray[i];
-                    UpdateElement(cell, 'hasNotName');
-                }
-                else {
-                    elementIsFound = true;
+                var currentCell = cellsCollectionArray[i];
+
+                UpdateElement(currentCell, null);
+
+                for (var j = 0; j < chemicalType.split(' ').length; j++) {
+                    currentCell.classList.add(chemicalType.split(' ')[j]);
                 }
             }
-        }
+        });
 
-        if (elementIsFound == false) {
-            ShowElementError(2);
-        }
-    });
+        elementInputField.addEventListener('blur', function (event) {
+            event.preventDefault();
 
-    darkModeBtn.addEventListener('click', function (event) {
-        event.preventDefault();
-        var textContent = darkModeBtn.textContent;
-        var header = document.querySelector('body > header');
-        if (textContent == 'Dark Mode') {
-            darkModeBtn.textContent = 'Light Mode'; 
-            UpdateElement(darkModeBtn, 'lightModeBtn');
-            UpdateElement(header, 'darkModeHeader');
-        }
-        else {
-            darkModeBtn.textContent = 'Dark Mode';
-            UpdateElement(darkModeBtn, 'darkModeBtn');
-            UpdateElement(header, 'lightModeHeader');
-        }
-    });
+            var elementIsFound = false;
+            var elementValue = elementInputField.value;
+
+            if (!elementInputField || elementInputField == '' || elementInputField == ' ') {
+                ShowElementError(2);
+            }
+            else {
+                for (var i = 0; i < cellsCollectionArray.length; i++) {
+
+                    var currName = values[i].Name;
+
+                    if (currName != elementValue) {
+                        var cell = cellsCollectionArray[i];
+                        UpdateElement(cell, 'hasNotName');
+                    }
+                    else {
+                        elementIsFound = true;
+                    }
+                }
+            }
+
+            if (elementIsFound == false) {
+                ShowElementError(2);
+            }
+        });
+
+        darkModeBtn.addEventListener('click', function (event) {
+            event.preventDefault();
+            var textContent = darkModeBtn.textContent;
+            var header = document.querySelector('body > header');
+            if (textContent == 'Dark Mode') {
+                darkModeBtn.textContent = 'Light Mode';
+                UpdateElement(darkModeBtn, 'lightModeBtn');
+                UpdateElement(header, 'darkModeHeader');
+            }
+            else {
+                darkModeBtn.textContent = 'Dark Mode';
+                UpdateElement(darkModeBtn, 'darkModeBtn');
+                UpdateElement(header, 'lightModeHeader');
+            }
+        });
+    }
+
+    var values = Array.from(model);
+    var select = document.getElementById('seeItemsList');
+
+    var cellsCollection = document.getElementsByTagName('td');
+
+    var cellsCollectionArray = Array.from(cellsCollection)
+        .filter(x => x.classList.length != 0).sort((a, b) =>
+            a.getElementsByClassName('atomicNumber')[0].textContent -
+            b.getElementsByClassName('atomicNumber')[0].textContent);
+
+    var yearInputField = document.getElementsByClassName('yearInput')[0];
+    var elementInputField = document.getElementsByClassName('elementInput')[0];
+    var darkModeBtn = document.getElementsByClassName('darkModeBtn')[0];
+
+    CreatePopUpMenu(cellsCollectionArray,values);
+   
+    CreateEventListeners(cellsCollectionArray, values,
+        select, yearInputField, elementInputField, darkModeBtn);
 }
