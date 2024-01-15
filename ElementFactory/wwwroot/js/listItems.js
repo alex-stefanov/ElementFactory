0dffec5c1d84053eb88ba035ed95bdd93fe64a7c
Stilyan <117260079+stilyan122@users.noreply.github.com>
2024-01-15 18:14:52 +0200

    Improved html and css structures in the main page

--- ElementFactory/wwwroot/js/listItems.js
+++ ElementFactory/wwwroot/js/listItems.js
@@ -10,12 +10,21 @@ function PageSettings(
     boilingPoints,
     years) {
 
-    function CreateDOMElement(tagName,parent,className) {
+    function CreateDOMElement(tagName, parent, className) {
         var element = document.createElement(tagName);
         element.classList.add(className);
         parent.innerHTML += element;
         return element;
     }
+
+    function UpdateElement(element, className) {
+        element.classList = [];
+        if (className != null) {
+            element.classList.add(className);
+        }
+        element.style = null;
+    }
+
     var cellsCollection = document.getElementsByTagName('td');
 
     var cellsCollectionArray = Array.from(cellsCollection)
@@ -23,9 +32,13 @@ function PageSettings(
             a.getElementsByClassName('atomicNumber')[0].textContent -
             b.getElementsByClassName('atomicNumber')[0].textContent);
 
+    var inputField = document.getElementsByClassName('yearInput')[0];
+
+    //TODO: -> structure
     for (var i = 0; i < cellsCollectionArray.length; i++) {
 
         var currentCell = cellsCollectionArray[i];
+
         currentCell.addEventListener('click', function (event) {
             event.preventDefault();
 
@@ -58,65 +71,137 @@ function PageSettings(
 
         })
     }
+    //
 
     var select = document.getElementById('seeItemsList');
 
     select
-        .addEventListener('change', function (event) {
-        event.preventDefault();
+    .addEventListener('change', function (event) {
+          event.preventDefault();
 
-        var selectedIndex = select.selectedIndex;
-        var selectedOption = select.options[selectedIndex].id;
+          var selectedIndex = select.selectedIndex;
+          var selectedOption = select.options[selectedIndex].id;
+          inputField.style.display = 'none';
 
+          for (var i = 0; i < cellsCollectionArray.length; i++) {
+
+                var currentCell = cellsCollectionArray[i];
+
+                if (selectedOption == 'seeAtomicNumber') {
+
+                    var chemicalType = chemicalTypeNames[i]
+                        .map(x => String.fromCharCode(x)).join('');
+
+                    UpdateElement(currentCell, null);
+
+                    for (var j = 0; j < chemicalType.split(' ').length; j++) { 
+                        currentCell.classList.add(chemicalType.split(' ')[j]);
+                    }
+                }
+
+                else if (selectedOption == 'seeAtomicMass') {
+ 
+                    var mass = atomicMasses[i] * 0.60;  
+                
+                    UpdateElement(currentCell, 'hasAtomicMass');
+                
+
+                    currentCell.style.backgroundColor = `rgba(115, ${235 - mass}, 255)`;
+
+                    currentCell.style.border = `3px solid rgba(115, ${160 - mass}, 255)`;
+                }
+
+                else if (selectedOption == 'seePhysicalAppearance') {
+
+                    var state = states[i]
+                        .map(x => String.fromCharCode(x)).join('');
+
+                    UpdateElement(currentCell, state);
+                }
+
+                else if (selectedOption == 'seeAtomicRadius') {
+
+                    var atomicRadius = parseFloat(atomicRadiuses[i]) / parseFloat(5);
+
+                    UpdateElement(currentCell, 'hasAtomicRadius');
+
+                    if (atomicRadius > 0) {
+                        currentCell.style.backgroundColor = `rgba(0, ${atomicRadius * 2.5}, 70)`;
+
+                        currentCell.style.border = `3px solid rgba(0, ${atomicRadius * 2.5}, 60)`;
+                    }
+                    else {
+
+                        currentCell.style.backgroundColor = `rgba(0, 62, 70)`;
+
+                        currentCell.style.border = `3px solid rgba(0, 52, 60)`;
+                    }
+                }
+
+                else if (selectedOption == 'seeMeltingPoint') {
+
+                    var meltingPoint = parseFloat(meltingPoints[i]) / parseFloat(31);
+
+                    UpdateElement(currentCell, 'hasMeltingPoint');
+
+                    currentCell.style.backgroundColor = `rgba(${meltingPoint * 2.5}, 30, 70)`;
+
+                    currentCell.style.border = `3px solid rgba(${meltingPoint * 4}, 30, 60)`;
+                }
+
+                else if (selectedOption == 'seeBoilingPoint') {
+                var boilingPoint = parseFloat(boilingPoints[i]) / parseFloat(31);
+
+                UpdateElement(currentCell, 'hasBoilingPoint');
+
+                currentCell.style.backgroundColor = `rgba(${boilingPoint}, 0, 0)`;
+
+                currentCell.style.border = `3px solid rgba(${boilingPoint * 0.6}, 0, 0)`;
+            }
+
+                else if (selectedOption == 'seeByYear') {
+                    inputField.style.display = 'inline-block';
+                    break;
+                } 
+            }
+    });
+
+    inputField.addEventListener('focus', function (event) {
         for (var i = 0; i < cellsCollectionArray.length; i++) {
+            var chemicalType = chemicalTypeNames[i]
+                .map(x => String.fromCharCode(x)).join('');
 
             var currentCell = cellsCollectionArray[i];
 
-            if (selectedOption == 'seeAtomicNumber') {
+            UpdateElement(currentCell, null);
 
-                var chemicalType = chemicalTypeNames[i]
-                    .map(x => String.fromCharCode(x)).join('');
+            for (var j = 0; j < chemicalType.split(' ').length; j++) {
+                currentCell.classList.add(chemicalType.split(' ')[j]);
+            }
+        }
+    });
 
-                currentCell.classList = [];
+    inputField.addEventListener('blur', function (event) {
+        event.preventDefault();
 
-                for (var j = 0; j < chemicalType.split(' ').length; j++) { 
-                    currentCell.classList.add(chemicalType.split(' ')[j]);
+        var yearValue = inputField.value;
+
+        if (!yearValue || yearValue < 0 || yearValue > 2024) {
+            var errorDiv = document.getElementsByClassName('errorDiv')[0];
+            errorDiv.style.display = 'block';
+
+            setTimeout(function () {
+                errorDiv.style.display = 'none';
+            }, 3000);
+        }
+        else {
+            for (var i = 0; i < cellsCollectionArray.length; i++) {
+                var currYear = years[i];
+                if (currYear > yearValue) {
+                    var cell = cellsCollectionArray[i];
+                    UpdateElement(cell, 'hasYear');
                 }
-
-                currentCell.style = null;
             }
-            else if (selectedOption == 'seeAtomicMass') {
- 
-                var mass = atomicMasses[i];                    
-                currentCell.classList = [];
-
-                if (mass > 255) {
-                    mass *= 0.9;
-                }
-
-                console.log(mass);
-
-                currentCell.style.backgroundColor = `rgba(130, ${Math.abs(mass - 255)}, 255)`;
-                currentCell.style.border = `4px solid rgba(60, ${Math.abs(mass - 255)}, 255)`;
-                currentCell.classList.add('hasAtomicMass');
-            }
-            else if (selectedOption == 'seePhysicalAppearance') {
-                var state = states[i]
-                    .map(x => String.fromCharCode(x)).join('');
-
-                currentCell.classList = [];
-                currentCell.style = null;
-                currentCell.classList.add(state);
-            }
-            else if (selectedOption == 'seeAtomicRadius') {
-                var atomicRadius = atomicRadiuses[i];
-            } else if (selectedOption == 'seeMeltingPoint') {
-                var meltingPoint = meltingPoints[i];
-            } else if (selectedOption == 'seeBoilingPoint') {
-                var boilingPoint = boilingPoints[i];
-            } else if (selectedOption == 'seeByYear') {
-                var year = years[i];
-            } 
         }
     });
 }
