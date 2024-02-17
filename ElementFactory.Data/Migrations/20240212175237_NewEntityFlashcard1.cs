1a92a5d42689465131795a7e45dc4060e5f1d8cc
alex <rlgalexbgto@gmail.com>
2024-02-17 22:34:13 +0200

    New functionallities

---
+++ ElementFactory.Data/Migrations/20240212175237_NewEntityFlashcard1.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElementFactory.Data.Migrations
+{
+    public partial class NewEntityFlashcard1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterTable(
+                name: "Flashcards",
+                comment: "Flashcard Class");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Flashcards",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                comment: "Flashcard Title",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Flashcards",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                comment: "Flashcard Content",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Flashcards",
+                type: "int",
+                nullable: false,
+                comment: "Flashcard identificator",
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Flashcards",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Flashcards");
+
+            migrationBuilder.AlterTable(
+                name: "Flashcards",
+                oldComment: "Flashcard Class");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Flashcards",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldComment: "Flashcard Title");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Flashcards",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000,
+                oldComment: "Flashcard Content");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Flashcards",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldComment: "Flashcard identificator")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
---
+++ ElementFactory/Controllers/FlashcardController.cs
@@ -0,0 +1,111 @@
+﻿using ElementFactory.Data;
+using ElementFactory.Data.Models;
+using ElementFactory.Models.Flashcard;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElementFactory.Controllers
+{
+    public class FlashcardController:Controller
+    {
+        private readonly ApplicationDbContext data;
+
+        private readonly UserManager<User> userManager;
+
+        public FlashcardController(ApplicationDbContext _data
+            ,UserManager<User> _userManager)
+        {
+            this.data = _data;
+            this.userManager = _userManager;
+        }
+
+        public async Task<IActionResult> ViewAllFlashcards() 
+        {
+            var user = await userManager.FindByNameAsync(User.Identity.Name);
+            if (user != null)
+            {
+                var flashcards = user.
+                 Flashcards
+                 .Where(x=>x.IsActive)
+                 .Select(fc => new FlashcardViewModel()
+                    {
+                      Title = fc.Title,
+                      Content = fc.Content,
+                    })
+                 .ToList();
+
+                return View(flashcards);
+            }
+            return RedirectToAction("Error");     
+        }
+
+        public IActionResult AddFlashcard()
+            => View();
+
+        [HttpPost]
+        public async Task<IActionResult> AddFlashcard(FlashcardViewModel model)
+        {
+            var flashcard = new Flashcard()
+            {
+                Title = model.Title,
+                Content = model.Content
+            };
+            var user = await userManager.FindByNameAsync(User.Identity.Name);
+            if(user != null)
+            {      
+                await data.Flashcards.AddAsync(flashcard);
+                user.Flashcards.Add(flashcard);
+                await data.SaveChangesAsync();
+                return RedirectToAction("ViewAllFlashcards");
+            }
+            return RedirectToAction("Error");
+            
+           
+        }
+
+        public async Task<IActionResult> EditFlashcard(int id)
+        {
+            var flashcard = await data.Flashcards.FindAsync(id);
+            if(flashcard != null)
+            {
+                return View(new FlashcardViewModel()
+                {
+                    Title = flashcard.Title,
+                    Content = flashcard.Content,
+                });
+            }
+            return View("Error"); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditFlashcard
+            (int id, FlashcardViewModel model)
+        {
+            var flashcard = await data.Flashcards.FindAsync(id);
+
+            if (flashcard != null)
+            {
+                flashcard.Title = model.Title;
+                flashcard.Content = model.Content;
+                await data.SaveChangesAsync();
+                return RedirectToAction("ViewAllFlashcards");
+            }
+            return View("Error");      
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteFlashcard(int id)
+        {
+            var flashcard= await data.Flashcards.FindAsync(id);
+
+            if (flashcard != null)
+            {
+                flashcard.IsActive = false;
+                return RedirectToAction("ViewAllFlashcards");
+            }
+            return View("Error");
+        }
+    }
+}
